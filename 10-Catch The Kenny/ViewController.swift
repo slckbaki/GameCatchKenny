3af061f4cf7326c338f9286c2affe10105c3727b
selcuk baki <selcukbaki@windowslive.com>
2021-08-18 00:37:49 +0300

    Initial Commit

---
+++ 10-Catch The Kenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10-Catch The Kenny
+//
+//  Created by Selcuk Baki on 18/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
